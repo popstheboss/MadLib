ad82f9db5c5bb39383e859d5a873dab8add82f45
Antonio Forrest <antonio.forrest.2023@student.lhschools.org>
2020-02-25 13:18:51 -0500

    Intellji Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // phase one
+        String nounWithLessThanSixLetters;
+        String color;
+        String animal;
+        String verbThatEndsInEd;
+        String food;
+        String famousPerson;
+        String numberMoreThanOneHundred;
+        String cityInUSA;
+        String favoriteMovie;
+        String nounOfSomethingYouWouldFindInSchool;
+        String favoriteDrink;
+        String roomInYourHouse;
+        String fruit;
+        String yourName;
+        String favoriteGame;
+        String verbThatEndsInIng;
+        String fastFoodRestaurant;
+        String IsFortniteAGoodGame;
+        String anyNoun;
+        String LeastFavoriteMovie;
+        String nounThatIsBlack;
+        Scanner keyboard;
+    }
+
+}
